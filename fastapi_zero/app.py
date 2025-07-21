5dd7672d8ca8de40a167dcf61f5994a85dc765af
Anderson Ferreira <havour@gmail.com>
2025-07-21 15:27:45 -0300

    aula 02

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,23 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fastapi_zero.schema import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'message': 'Ola Mundo!'}
+    return {'message': 'Olá Mundo!'}
+
+
+@app.get('/ex_aula_02', response_class=HTMLResponse)
+def ex_aula_02():
+    return """
+    <html>
+      <body>
+        <h1> Olá Mundo! </h1>
+      </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -7,6 +7,17 @@ from fastapi_zero.app import app
 
 def test_root_deve_retornar_ola_mundo_e_ok():
     client = TestClient(app)
-    response = client.get("/")
-    assert response.json() == {"message": "Ola Mundo!"}
+
+    response = client.get('/')
+
+    assert response.json() == {'message': 'Olá Mundo!'}
     assert response.status_code == HTTPStatus.OK
+
+
+def test_ex_aula_02_ola_mundo_em_html():
+    client = TestClient(app)
+
+    response = client.get('/ex_aula_02')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo! </h1>' in response.text
