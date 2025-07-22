bd56ed8a8f20cb98364e7903a41c3e010ef274c4
Anderson Ferreira <havour@gmail.com>
2025-07-22 15:41:02 -0300

    Aula 04 - Banco de Dados com SQLAlchemy e Gerenciando Migrações com Alembic + Exercicio

--- fastapi_zero/exercicio.py
+++ fastapi_zero/exercicio.py
@@ -1,13 +1,24 @@
 # fastapi_zero/exercicios.py
 
-from fastapi import APIRouter
+from http import HTTPStatus
+
+from fastapi import APIRouter, HTTPException
 from fastapi.responses import HTMLResponse
 
-router = APIRouter()
+from fastapi_zero.schemas import (
+    Message,
+    UserDB,
+    UserPublic,
+    UserSchema,
+)
+
+router = APIRouter(prefix='/ex', tags=['exercicios'])
+
+database_ex = []
 
 
 # Use o decorator do router
-@router.get('/ex_aula_02', response_class=HTMLResponse)
+@router.get('/aula_02', response_class=HTMLResponse)
 def ex_aula_02():
     return """
     <html>
@@ -15,3 +26,49 @@ def ex_aula_02():
         <h1> Olá Mundo! </h1>
       </body>
     </html>"""
+
+
+@router.post(
+    '/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic
+)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database_ex) + 1)
+
+    database_ex.append(user_with_id)
+
+    return user_with_id
+
+
+@router.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database_ex) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database_ex[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@router.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database_ex) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    del database_ex[user_id - 1]
+
+    return {'message': 'User deleted'}
+
+
+@router.get('/users/{user_id}', response_model=UserPublic)
+def read_user__exercicio(user_id: int):
+    if user_id > len(database_ex) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    return database_ex[user_id - 1]
--- tests/test_exercicios.py
+++ tests/test_exercicios.py
@@ -2,7 +2,62 @@ from http import HTTPStatus
 
 
 def test_ex_aula_02_ola_mundo_em_html(client):
-    response = client.get('/ex_aula_02')
+    response = client.get('/ex/aula_02')
 
     assert response.status_code == HTTPStatus.OK
     assert '<h1> Olá Mundo! </h1>' in response.text
+
+
+def test_create_user_exercicio(client):
+    response = client.post(
+        '/ex/users/',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_get_user___exercicio(client):
+    response = client.get('/ex/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user_should_return_not_found__exercicio(client):
+    response = client.delete('/ex/users/666')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_update_user_should_return_not_found__exercicio(client):
+    response = client.put(
+        '/ex/users/666',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_get_user_should_return_not_found__exercicio(client):
+    response = client.get('/ex/users/666')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
